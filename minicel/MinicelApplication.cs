9a7c2c0b4570ac988be4e25c368edd18b125db4b
SZE19728 <SZE19728@spengergasse.at>
2021-12-29 23:20:57 +0100

    csv update

--- minicel/MinicelApplication.cs
+++ minicel/MinicelApplication.cs
@@ -9,15 +9,14 @@ namespace minicel
     class MinicelApplication
     {
 
-        int xPos = 0;
-        int yPos = 0;
+        int colPos = 0;
+        int rowPos = 0;
 
         string last = "";
 
-        
         enum State
         {
-            CommandMode, 
+            CommandMode,
             InsertMode,
             MovementMode
         }
@@ -26,16 +25,55 @@ namespace minicel
         List<List<string>> cells = new List<List<string>>();
         bool isFirstLoop = true;
 
-        
+
         ConsoleKeyInfo consoleKey;
-        public MinicelApplication(List<string> content)
+
+
+        void DrawRows()
         {
-            foreach (var item in content)
+            for (int row = 0; row < 26; row++)
             {
-                string[] s = item.Split(',');
-                List<string> sl = s.ToList<string>();
-                cells.Add(sl);
+                string rowf = row.ToString();
+                string res = "";
+                int length = 3;
+                for (int i = 0; i < length - rowf.Length; i++)
+                {
+                    res += " ";
+                }
+                res += row.ToString();
+                Console.Write(res + "\n");
             }
+        }
+        void DrawCols()
+        {
+            ConsoleAppearance.ResetColors();
+            ConsoleAppearance.GreenBGBlackFG();
+            Console.Write("   ");
+            for (int column = 65; column < 78; column++)
+            {
+                Console.Write((char)(column) + "       " + (column != 77 ? "|" : ""));
+            }
+        }
+
+        void DrawMode() {
+            ConsoleAppearance.ResetColors();
+            string s = currentstate switch
+            {
+                State.InsertMode => "--INSERT--",
+                State.CommandMode => "--COMMAND--",
+                State.MovementMode => "--MOVE--",
+                // _ => "--MOVE--"
+
+            };
+            ConsoleAppearance.BlackBGGreenFG();
+            if (s != last)
+                Console.WriteLine("\t" + s);
+            last = s;
+        }
+
+
+        void Loop()
+        {
             while (true)
             {
                 if (!isFirstLoop)
@@ -43,34 +81,17 @@ namespace minicel
                     consoleKey = Console.ReadKey();
                     inputChar = consoleKey.KeyChar;
                 }
-               ConsoleAppearance.ResetColors();
-                if(currentstate == State.MovementMode)
+                ConsoleAppearance.ResetColors();
+                if (currentstate == State.MovementMode)
                 {
 
-                Console.Clear();
-                    ConsoleAppearance.ResetColors();
-                    ConsoleAppearance.GreenBGBlackFG();
-                Console.Write("   ");
-                for (int column = 65; column < 78; column++)
-                {
-                    Console.Write((char)(column)+"       "+(column != 77 ? "|": ""));
-                }
+                    Console.Clear();
 
-                
-                Console.Write("\n");
+                    DrawCols();
+                    DrawRows();
+                    Console.Write("\n");
                     ConsoleAppearance.GreenBGBlackFG();
-                for (int row = 0;  row< 26; row++)
-                {
-                    string rowf = row.ToString();
-                    string res = "";
-                    int length = 3;
-                    for(int i = 0; i< length-rowf.Length; i++)
-                    {
-                        res += " ";
-                    }
-                    res += row.ToString();
-                    Console.Write(res+"\n");
-                }
+                    
                 }
 
 
@@ -108,22 +129,26 @@ namespace minicel
                     }
                 }
 
-                ConsoleAppearance.ResetColors();
-                string s = currentstate switch
-                {
-                    State.InsertMode => "--INSERT--",
-                    State.CommandMode => "--COMMAND--",
-                    State.MovementMode => "--MOVE--",
-                   // _ => "--MOVE--"
-
-                };
-                ConsoleAppearance.BlackBGGreenFG();
-                if(s != last)
-                    Console.WriteLine("\t" + s);
-                last = s;
+                DrawMode();
                 isFirstLoop = false;
             }
         }
 
+        void FillCells(List<string> content)
+        {
+            foreach (var item in content)
+            {
+                string[] s = item.Split(';');
+                List<string> sl = s.ToList<string>();
+                cells.Add(sl);
+            }
+        }
+        public MinicelApplication(List<string> content)
+        {
+            FillCells(content);
+            Loop();
+
+
+        }
     }
 }
