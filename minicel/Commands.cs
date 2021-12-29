d11c8e6e973c7aa4da89a17f2404e36147135c78
SZE19728 <SZE19728@spengergasse.at>
2021-12-29 23:03:34 +0100

    pre refactor commit

---
+++ minicel/Commands.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace minicel
+{
+    public static class Commands
+    {
+        public delegate void Command(List<object> cells);
+        //public class CommandItem
+        //{
+        //    string _commandName;
+        //    public string CommandName { get { return _commandName; } }
+        //    Command _effect;
+        //    public Command Effect { get { return _effect; } }
+        //    public CommandItem(string commandName, Command effect)
+        //    {
+        //        _effect = effect;
+        //        _commandName = commandName;
+        //    }
+        //}
+
+        static Dictionary<string, Command> commandList = new Dictionary<string, Command>()
+        {
+            {"w", content => {
+                StreamWriter sw = new StreamWriter((string)(content[0]));
+                
+            }}
+        };
+
+    }
+}
--- minicel/MinicelApplication.cs
+++ minicel/MinicelApplication.cs
@@ -12,6 +12,8 @@ namespace minicel
         int xPos = 0;
         int yPos = 0;
 
+        string last = "";
+
         ConsoleColor foreground = Console.ForegroundColor;
         ConsoleColor background = Console.BackgroundColor;
         enum State
@@ -20,8 +22,10 @@ namespace minicel
             InsertMode,
             MovementMode
         }
+        char inputChar = ' ';
         State currentstate = State.MovementMode;
         List<List<string>> cells = new List<List<string>>();
+        bool isFirstLoop = true;
 
         public void GreenBGBlackFG()
         {
@@ -37,6 +41,7 @@ namespace minicel
             Console.ForegroundColor = foreground;
             Console.BackgroundColor = background;
         }
+        ConsoleKeyInfo consoleKey;
         public MinicelApplication(List<string> content)
         {
             foreach (var item in content)
@@ -47,7 +52,15 @@ namespace minicel
             }
             while (true)
             {
+                if (!isFirstLoop)
+                {
+                    consoleKey = Console.ReadKey();
+                    inputChar = consoleKey.KeyChar;
+                }
                 ResetColors();
+                if(currentstate == State.MovementMode)
+                {
+
                 Console.Clear();
                 ResetColors();
                 GreenBGBlackFG();
@@ -72,25 +85,57 @@ namespace minicel
                     res += row.ToString();
                     Console.Write(res+"\n");
                 }
+                }
+
+
+                if (currentstate != State.CommandMode)
+                {
+                    switch (inputChar)
+                    {
+                        case 'q':
+                            ResetColors();
+                            Console.Clear();
+                            Environment.Exit(0);
+                            break;
+                        case ':':
+                            currentstate = State.CommandMode;
+                            break;
+                        case 'i':
+                        case '=':
+                            currentstate = State.InsertMode;
+                            break;
+                        case 'h':
+                        case 'j':
+                        case 'k':
+                        case 'l':
+                            Console.Clear();
+                            currentstate = State.MovementMode;
+                            break;
+
+                    }
+                }
+                else
+                {
+                    if (consoleKey.Key == ConsoleKey.Enter)
+                    {
+
+                    }
+                }
+
                 ResetColors();
-                string s = currentstate switch 
+                string s = currentstate switch
                 {
                     State.InsertMode => "--INSERT--",
-                    State.CommandMode=> "--COMMAND--",
-                    State.MovementMode=> "--MOVE--", 
-                    _ => "--MOVE--"
+                    State.CommandMode => "--COMMAND--",
+                    State.MovementMode => "--MOVE--",
+                   // _ => "--MOVE--"
 
                 };
                 BlackBGGreenFG();
-                Console.WriteLine("\t"+s);
-
-                char inputChar = Console.ReadKey().KeyChar;
-                if(inputChar == 'q')
-                {
-                    ResetColors();
-                    Console.Clear();
-                    Environment.Exit(0);
-                }
+                if(s != last)
+                    Console.WriteLine("\t" + s);
+                last = s;
+                isFirstLoop = false;
             }
         }
 
